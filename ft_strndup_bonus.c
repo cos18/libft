d1ab97d7b0a81d8ed509ecc5472c4c8cd20fcd0a
Sungwoo Park <codest99@gmail.com>
2020-03-14 23:36:35 +0900

    Add strndup function

--- ft_strndup_bonus.c
+++ ft_strndup_bonus.c
@@ -1,24 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft_bonus.h                                      :+:      :+:    :+:   */
+/*   ft_strndup_bonus.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sunpark <sunpark@student.42.kr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/26 21:06:08 by sunpark           #+#    #+#             */
-/*   Updated: 2020/03/14 23:23:05 by sunpark          ###   ########.fr       */
+/*   Created: 2020/03/14 23:23:49 by sunpark           #+#    #+#             */
+/*   Updated: 2020/03/14 23:35:44 by sunpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_BONUS_H
-# define LIBFT_BONUS_H
+#include "libft.h"
+#include "libft_bonus.h"
 
-# include <stdlib.h>
-
-int		ft_isupper(int c);
-int		ft_islower(int c);
-int		ft_isnumber(int c);
-int		ft_isblank(int c);
-char	*ft_strndup(const char *s1, size_t n);
-
-#endif
+char	*ft_strndup(const char *s1, size_t n)
+{
+	size_t	newsize;
+	char	*result;
+	char	*now;
+	
+	newsize = ft_strlen(s1);
+	newsize = (newsize > n) ? n : newsize;
+	result = (char *)malloc(newsize + 1);
+	if (result)
+	{
+		now = result;
+		while (newsize--)
+			*(now++) = *(s1++);
+		*now = '\0';
+	}
+	return (result);
+}
